120eb4bcb14f9ccec98c1049a40edf4630c37e8d
laucalop <laura.zazpi@gmail.com>
2017-12-13 09:56:46 +0100

    Módulos y Componentes

---
+++ Proyecto-componentes/src/app/share/share.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PieComponent } from './pie/pie.component';
+import { CabeceraComponent } from './cabecera/cabecera.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    CabeceraComponent,
+    PieComponent
+  ]
+})
+export class ShareModule { }
